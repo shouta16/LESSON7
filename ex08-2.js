b21b57dc3a677df3020f24ec8290584a454dfe02
shota <koutasukun@icloud.com>
2020-05-31 17:18:01 +0900

    第６回

--- ex08-2.js
+++ ex08-2.js
@@ -2,3 +2,17 @@ const Title = document.querySelector('#title')
 console.log(Title.textContent);
 
 
+
+/*<body>
+     <section id="title">
+          <h1>DOM </h1>
+           <p>JavaScriptからHTMLを扱うための仕組み</p>
+           <h2>印象に残っているトピック</h2>
+           <ul>
+               <li>documentオブジェクト</li>
+                <li>getElementById</li>
+                 <li>イベントリスト</li>
+            </ul>
+     </section>
+</body>
+<script src="ex08-2.js"></script> */
