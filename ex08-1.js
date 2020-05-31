d8427314d1a3875212541834978081934c5e9d68
shota <koutasukun@icloud.com>
2020-05-31 17:54:41 +0900

    第６回

--- ex08-1.js
+++ ex08-1.js
@@ -10,27 +10,3 @@ const list = document.querySelector('.list-item')
 console.log(list.textContent);
 
 
-
-
-/*<section id="kaimono">
-    <h1>買い物リスト</h1>
-    <p>
-        買い物のリストです。
-        <a href="http://example.com/">リンク</a>
-    </p>
-
-    <ul class="list">
-        <li>りんご</li>
-        <li>みかん</li>
-        <li>バナナ</li>
-    </ul>
-</section>
-
-<section id="todo">
-    <h1 class="todo-item">TODOリスト</h1>
-
-    <ul class="list">
-        <li>DOMを把握する</li>
-        <li class="list-item">課題をやる</li>
-    </ul>
-</section> */
